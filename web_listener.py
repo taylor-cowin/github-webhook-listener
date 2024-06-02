a8218c7520e39e66f2a7394f8e62cbcf64ae5112
Taylor Cowin <80193327+taylor-cowin@users.noreply.github.com>
2024-06-01 22:42:17 -0500

    parsing json sucks

--- web_listener.py
+++ web_listener.py
@@ -5,26 +5,28 @@ import logging
 
 #Global
 logger=None
-config_json=None
+config_json={}
 settings_file="./settings.json"
 
 def ensure_logger():
     global logger
     if logger is None:
         logger = logging.getLogger(__name__)
-        logging.basicConfig(filename='chatgpt-discord-bot.log', level=logging.INFO,
+        logging.basicConfig(filename='github-updater.log', level=logging.INFO,
                             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         
 def main():
     ensure_logger()
     with open(settings_file, "r") as _f:
         try:
-            config_json = json.load(_f.read())
+            global config_json
+            config_json = json.load(_f)
         except IOError:
             logger.warning(f"Couldn't load {settings_file}")
+            print(f"Couldn't load {settings_file}")
         finally:
-            if config_json is not None:
-                #TODO
-                return
+            if config_json != {}:
+                print(config_json['repos'][0]['endpoints'][0]['local_dir'])
+                          
 if __name__ == "__main__":
     main()            
