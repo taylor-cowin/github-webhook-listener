b37147d5f4e3bcf0d5422433da4c7e5d06ab6a0d
Taylor Cowin <80193327+taylor-cowin@users.noreply.github.com>
2024-06-11 17:22:32 -0500

    added web listener to reload script

--- web_listener.py
+++ web_listener.py
@@ -22,15 +22,15 @@ def ensure_logger():
 
 def restart_service(service_name):
     result = subprocess.run(str(f"sudo systemctl restart {service_name}.service"), capture_output = True, text = True, shell=True)
-    logger.debug(f"Restart Server - Result: {result.stdout}")
-    logger.debug(f"Restart Server - Error: {result.stderr}")
+    logger.debug(f"Restart {service_name} - Result: {result.stdout}")
+    logger.debug(f"Restart {service_name} - Error: {result.stderr}")
 
 def git_command(endpoint):
     github_url=str(f"https://github.com/{endpoint.remote_user}/{endpoint.repo_name} {endpoint.remote_name} {endpoint.remote_branch}")                      
     working_dir=str(f"{endpoint.local_dir}{endpoint.repo_name}")
     result = subprocess.run(str(f"cd {working_dir} && git fetch && git pull {github_url}"), capture_output = True, text = True, shell=True)
-    logger.debug(f"Git Command - Result: {result.stdout}")
-    logger.debug(f"Git Command - Error: {result.stderr}")
+    logger.debug(f"Git {endpoint} - Result: {result.stdout}")
+    logger.debug(f"Git {endpoint} - Error: {result.stderr}")
 
     restart_service(endpoint.service_name)
 
