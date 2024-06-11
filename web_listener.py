3754c69d270dd9589909b76f3b3a19f8ced6c7ef
Taylor Cowin <80193327+taylor-cowin@users.noreply.github.com>
2024-06-11 17:46:49 -0500

    added web listener to reload script

--- web_listener.py
+++ web_listener.py
@@ -57,6 +57,7 @@ class EndpointHandler:
                 if branch == endpoint.remote_branch:
                     git_command(endpoint)
 
+
 class EndpointClass:
     def __init__(self, endpoint, repo_name, local_dir, remote_user, remote_name, remote_branch, service_name):
         self.endpoint = endpoint
