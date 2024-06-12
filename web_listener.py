a140a4fae16c3414cdc28ea96080abe46926bac1
Taylor Cowin <taylorcowin@protonmail.com>
2024-06-11 23:25:18 -0500

    I think this oughta do it.

--- web_listener.py
+++ web_listener.py
@@ -27,11 +27,11 @@ def restart_service(service_name):
     logger.debug(f"Restart {service_name} - Error: {result.stderr}")
 
 def git_command(endpoint):
-    github_url=str(f"https://github.com/{endpoint.remote_user}/{endpoint.repo_name} {endpoint.remote_name} {endpoint.remote_branch}")                      
+    #github_url=str(f"https://github.com/{endpoint.remote_user}/{endpoint.repo_name}")                      
     working_dir=str(f"{endpoint.local_dir}")
-    logger.debug(f"Github URL: {github_url}")
+    #logger.debug(f"Github URL: {github_url}")
     logger.debug(f"Working Dir: {working_dir}")
-    result = subprocess.run(str(f"cd {working_dir} && git fetch && git pull {github_url}"), capture_output = True, text = True, shell=True)
+    result = subprocess.run(str(f"cd {working_dir} && git fetch && git pull {endpoint.remote_name} {endpoint.remote_branch}"), capture_output = True, text = True, shell=True)
     logger.debug(f"Git {endpoint} - Result: {result.stdout}")
     logger.debug(f"Git {endpoint} - Error: {result.stderr}")
 
